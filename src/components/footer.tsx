da7d0edd433cb766345cb1819a0aa6b9a6c417cc
Kazushi Kawamura <wakaka2277@gmail.com>
2019-09-03 14:21:10 +0900

    feat: add github link to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,15 @@
 import React, { memo } from "react";
 
+import { GithubIcon } from "../icons/github";
+
 const Footer = memo(({}) => (
   <div className="bg-black text-gray-200 text-xs py-2 mt-4">
-    <div className="max-w-main m-auto px-8">2019 © Kazushi Kawamura.</div>
+    <div className="max-w-main m-auto px-8 flex justify-between items-center">
+      <div>2019 © Kazushi Kawamura.</div>
+      <a target="_blank" href="https://github.com/kawamurakazushi/.com">
+        <GithubIcon size="18" />
+      </a>
+    </div>
   </div>
 ));
 
