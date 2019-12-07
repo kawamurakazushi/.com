25981b33dc8415c1bb86fc91da189ed61e3ae3c3
Kazushi Kawamura <wakaka2277@gmail.com>
2019-12-08 00:25:18 +0900

    feat: about me

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -5,10 +5,33 @@ import Layout from "../components/layout";
 const MePage = () => {
   return (
     <Layout>
-      <h1 className="font-thin">ABOUT ME</h1>
-      <p>
-        Hi, I'm Kazushi Kawamura working as a Software Engineer based in Tokyo.
-      </p>
+      <h1 className="heading mb-4">Hello.</h1>
+      <div className="text-sm">
+        <p className="my-2">I’m a Software Engineer living in Tokyo.</p>
+        <p className="my-2">
+          I enjoy cooking curry 🍛 and also maintain an app called currylife, a
+          online community for curry lovers.
+        </p>
+        <p className="my-2">
+          Please feel free to DM me on{" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="https://twitter.com/kawamurakazush1"
+          >
+            twitter
+          </a>{" "}
+          or send{" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="mailto:me@kawamurakazushi.com"
+          >
+            message
+          </a>{" "}
+          :)
+        </p>
+      </div>
     </Layout>
   );
 };
