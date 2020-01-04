53dd10a2e5fa2b798454da4ff741185ffa9f55f3
Kazushi Kawamura <wakaka2277@gmail.com>
2020-01-04 19:03:42 +0900

    style: fix book item

--- src/components/bookItem.tsx
+++ src/components/bookItem.tsx
@@ -12,16 +12,16 @@ interface Props {
 const BookItem: FC<Props> = ({ author, cover, isbn, title, readAt }) => {
   return (
     <Link to={`/books/${isbn}`} className="flex mb-5">
-      <div className="w-10">
+      <div className="w-16">
         <img src={cover} />
       </div>
       <div className="flex flex-col ml-2 flex-1">
-        <div className="font-medium mr-2 hover:bg-black hover:text-white">
-          {title}
+        <div className="font-medium mr-2">
+          <div className="hover:bg-black hover:text-white inline">{title}</div>
         </div>
         <div className="font-thin text-xs mt-1">{author}</div>
       </div>
-      <div className="text-xs mt-1 ml-auto">
+      <div className="text-xs mt-1 whitespace-no-wrap">
         {readAt ? (
           <div className="">{readAt}</div>
         ) : (
