6a5b9c63199fade3e11f129b0636466b4943e7dd
Kazushi Kawamura <wakaka2277@gmail.com>
2020-01-14 21:26:45 +0900

    fix: nested link

--- src/components/articleItem.tsx
+++ src/components/articleItem.tsx
@@ -26,26 +26,28 @@ const ArticleItem = memo(
     }
 
     return (
-      <Link className="flex mb-5" to={to}>
-        <div className="flex-1">
-          <h3 className="inline text-base font-medium hover:bg-black hover:text-white">
-            {title}
-          </h3>
-          {excerpt && <div className="text-xs mt-2 mb-1">{excerpt}</div>}
-          <div className="flex">
-            {tags && (
-              <>
-                {tags.map(tag => (
-                  <Link key={tag} to={`/tags/${tag}`} className="text-xs mr-1">
-                    {`#${tag}`}
-                  </Link>
-                ))}
-              </>
-            )}
-            <div className="text-xs text-gray-400">ー {date}</div>
+      <div className="mb-5">
+        <Link className="flex" to={to}>
+          <div className="flex-1">
+            <h3 className="inline text-base font-medium hover:bg-black hover:text-white">
+              {title}
+            </h3>
+            {excerpt && <div className="text-xs mt-2 mb-1">{excerpt}</div>}
           </div>
+        </Link>
+        <div className="flex">
+          {tags && (
+            <>
+              {tags.map(tag => (
+                <Link key={tag} to={`/tags/${tag}`} className="text-xs mr-1">
+                  {`#${tag}`}
+                </Link>
+              ))}
+            </>
+          )}
+          <div className="text-xs text-gray-400">ー {date}</div>
         </div>
-      </Link>
+      </div>
     );
   }
 );
